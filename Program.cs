6676b54a18f4ce2d797cba1c516117c9a890086c
Hugo Cardoso <hugorafaelmc@gmail.com>
2021-06-27 18:37:31 -0300

    Finished development

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("== Input time: ==");
+            Console.WriteLine("(Example: 1m, 35s)");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring((data.Length - 1), 1));
+            int time = int.Parse(data.Substring(0, (data.Length - 1)));
+            int multiplier = type == 's' ? 1 : 60;
+
+            Start(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+            PreStart();
+
+            int currentTime = 1;
+
+            while (currentTime <= time)
+            {
+                Console.Clear();
+                Console.WriteLine(currentTime);
+                currentTime++;
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Press any key to return.");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void PreStart()
+        {
+            int currentTime = 3;
+
+            while (currentTime != 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Starting in...{ currentTime }");
+                currentTime--;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
